c6f6080d35d89fb8f2c3ed90e74f40e32d7edc2b
Artem Manchenkov <artyom@manchenkoff.me>
2019-01-28 02:10:48 +0300

    lesson 05 solution

--- lesson05/solution/_normal.py
+++ lesson05/solution/_normal.py
@@ -13,3 +13,55 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из __easy.py
+from lesson05.solution.__easy import create_directory, remove_directory, list_dir
+import os
+
+intro_message = """
+Welcome!
+
+Actions:
+go     - change directory
+list   - get content of current directory
+remove - remove selected folder
+make   - create new folder
+"""
+
+print(intro_message)
+
+
+def go_to(path):
+    try:
+        os.chdir(path)
+        print('Successfully changed directory')
+    except:
+        print('Error with changing directory')
+
+
+COMMANDS = {
+    'go': go_to,
+    'list': list_dir,
+    'make': create_directory,
+    'remove': remove_directory,
+}
+
+
+def get_command():
+    cmd = input("Type command: ")
+
+    if COMMANDS.get(cmd):
+        path = os.path.curdir
+
+        action = COMMANDS[cmd]
+
+        if cmd != 'list':
+            sub_path = input("Directory path: ")
+            path = os.path.join(path, sub_path)
+
+        action(path)
+    else:
+        print("Invalid command")
+
+    get_command()
+
+
+get_command()
